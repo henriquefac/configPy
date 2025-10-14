b17764800a69d622fd4e8f5c8d8e806547a58b59
henriquefac <henriquefac@edu.unifor.br>
2025-10-14 13:04:27 -0300

    Agora acesso a _openai é feito com um classmethod

--- scripts/env.py
+++ scripts/env.py
@@ -49,17 +49,18 @@ class OpenAIENV:
 class EnvManager:
     _instance: "EnvManager|None" = None
     _lock: Lock = Lock()
-    openai : OpenAIENV
+    _openai : OpenAIENV
     def __new__(cls):
         if cls._instance is None:
             with cls._lock:
                 if cls._instance is None:
                     cls._instance = super().__new__(cls)
                     # Initialize OpenAIENV singleton
-                    cls._instance.openai = OpenAIENV.get()
+                    cls._openai = OpenAIENV.get()
         return cls._instance
 
-    @property
-    def openai_env(self) -> OpenAIENV:
-        """Access the OpenAI environment singleton."""
-        return self.openai    
+    @classmethod
+    def openai_env(cls) -> OpenAIENV:
+        if cls._openai is None:
+            cls()
+        return cls._openai    
